cdf42c476025ccd97aa2511b44a523e7578bdeef
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-05-19 17:57:21 +0200

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import bodyParser from 'body-parser';
+import routes from './routes/routes';
+import { generateFakeData } from './utils/cron';
+import HttpException from './models/http-exception.model';
+import swaggerDocument from '../docs/swagger.json';
+
+const app = express();
+
+/**
+ * App Configuration
+ */
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(routes);
+
+// Serves images
+app.use(express.static('public'));
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ status: 'API is running on /api' });
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+/* eslint-disable */
+app.use((err: Error | HttpException, req: Request, res: Response, next: NextFunction) => {
+  // @ts-ignore
+  if (err && err.name === 'UnauthorizedError') {
+    return res.status(401).json({
+      status: 'error',
+      message: 'missing authorization credentials',
+    });
+    // @ts-ignore
+  } else if (err && err.errorCode) {
+    // @ts-ignore
+    res.status(err.errorCode).json(err.message);
+  } else if (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+generateFakeData();
+
+/**
+ * Server activation
+ */
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.info(`server up on port ${PORT}`);
+});
